2a62b537e4fffd5f94393065332212c19fabad95
Spau <110257115+spaudark@users.noreply.github.com>
2025-08-03 22:25:16 +0700

    Update contractAddresses.ts

--- consts/contractAddresses.ts
+++ consts/contractAddresses.ts
@@ -1,6 +1,6 @@
 export const nftDropContractAddress =
-  "0x578541c1D991074E29f10Be24DB07bb1F6e53aA7";
+  "0x";
 export const tokenContractAddress =
-  "0x5e9e00b3B1cE0b1C6eaf4477b30E6E63D8529cFa";
+  "0x";
 export const stakingContractAddress =
-  "0x6041D19dCa59a7a94a450ED6158042C9692fAD43";
+  "0x1";
